db2d454f925d2c59586f7bff0bb81b59f0d9f68e
Brad Akemi <mirado.akemi@gmail.com>
2023-07-16 19:26:31 +0300

    Test vue
    erreur sur "useState"

--- client/src/app/MyComponent.tsx
+++ client/src/app/MyComponent.tsx
@@ -2,36 +2,36 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 interface Product {
-    name: string;
-    price: number;
+  name: string;
+  price: number;
 }
 
 function MyComponent() {
-    const [products, setProducts] = useState<Product[]>([]);
+  const [products, setProducts] = useState<Product[]>([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get('http://localhost:8000/api/products/');
-                setProducts(response.data.products);
-            } catch (error) {
-                console.error(error);
-            }
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/api/products/');
+        setProducts(response.data.products);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
-    return (
-        <div>
-            {products.map((product, index) => (
-                <div key={index}>
-                    <h3>{product.name}</h3>
-                    <p>{product.price}</p>
-                </div>
-            ))}
+  return (
+    <div>
+      {products.map((product, index) => (
+        <div key={index}>
+          <h3>{product.name}</h3>
+          <p>{product.price}</p>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
 
 export default MyComponent;
