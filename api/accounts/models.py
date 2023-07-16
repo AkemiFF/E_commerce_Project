c2ea8f9d55ef9f36fd79403031a7059573c6d1ee
Brad Akemi <mirado.akemi@gmail.com>
2023-07-16 10:53:26 +0300

    Gestion de clients
    mise en place de la base de donnée pour les comptes clients

--- api/accounts/models.py
+++ api/accounts/models.py
@@ -2,17 +2,10 @@ from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 
-# class User(AbstractUser):
-#     pass
-
+class Shopper(AbstractUser):
+    pass
 
 # class Shopper(models.Model):
-#     user = models.OneToOneField(
-#         User, on_delete=models.CASCADE, related_name='shopper')
-#     groups = models.ManyToManyField(Group, blank=True)
-#     user_permissions = models.ManyToManyField(Permission, blank=True)
-
-class Shopper(models.Model):
-    name = models.CharField(max_length=45, blank=False, default="")
-    email = models.CharField(max_length=45, null=True, blank=True)
-    phone_number = models.IntegerField(null=True, blank=True)
+#     name = models.CharField(max_length=45, blank=False, default="")
+#     email = models.CharField(max_length=45, null=True, blank=True)
+#     phone_number = models.IntegerField(null=True, blank=True)
