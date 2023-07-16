b5f6f361a345dbd90565b36012564209a1af09c4
Brad Akemi <mirado.akemi@gmail.com>
2023-07-16 11:24:23 +0300

    Mise en place de django-cors-headers

--- api/E_commerce/settings.py
+++ api/E_commerce/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'shop',
     'accounts',
 ]
@@ -50,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'E_commerce.urls'
@@ -130,3 +133,11 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "accounts.Shopper"
+CORS_ORIGIN_ALLOW_ALL = True
+
+
+# CORS_ORIGIN_WHITELIST = [
+#     'http://localhost:3000',  # L'URL de votre application Next.js
+#     'http://example.com',
+#     # Autres URL autorisées
+# ]
--- api/shop/admin.py
+++ api/shop/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from shop.models import Product, Order, Cart, Invoice
+
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(Cart)
+admin.site.register(Invoice)
